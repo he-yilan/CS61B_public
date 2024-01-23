31054b29c43ff2993eea19264d3ede895b822b79
Elana Ho <elanaho@gmail.com>
2024-01-22 17:26:02 -0800

    assert that

--- CS61Bakery/src/bug/CS61BakeryTest.java
+++ CS61Bakery/src/bug/CS61BakeryTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static com.google.common.truth.Truth.assertWithMessage;
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.*;
 
 public class CS61BakeryTest {
@@ -12,19 +14,20 @@ public class CS61BakeryTest {
     public void restock() {
         CS61Bakery CS61B = new CS61Bakery(5, 4);
         CS61B.restock(2);
-        assertEquals(7, CS61B.getStock());
+        assertWithMessage("incorrect stock").that(CS61B.getStock()).isEqualTo(7);
+        assertThat(CS61B.getStock()).isEqualTo(7);
     }
 
     @Test
     public void buy() {
         CS61Bakery CS61B = new CS61Bakery(5, 4);
-        assertEquals(5, CS61B.buy(20));
+        assertThat(CS61B.buy(20)).isEqualTo(5);
     }
 
     @Test
     public void restockAndBuy() {
         CS61Bakery CS61B = new CS61Bakery(5, 4);
-        assertEquals(10, CS61B.restockAndBuy(5, 40));
+        assertThat(CS61B.restockAndBuy(5, 40)).isEqualTo(10);
     }
 
     @Test
@@ -37,6 +40,6 @@ public class CS61BakeryTest {
         CS61B.restock(100);
         CS61B.buy(64);
         CS61B.buy(33);
-        assertEquals(16, CS61B.getMaxPurchase());
+        assertThat(CS61B.getMaxPurchase()).isEqualTo(16);
     }
 }
